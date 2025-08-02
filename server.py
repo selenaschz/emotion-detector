24bbc2a30d40b6dd15100b55c4e0977805e30888
selenaschz <sele71098@gmail.com>
2025-08-02 16:25:55 +0200

    Add Flask server with emotion detection route and index rendering

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("EmotionDetector")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+
+    emotions = ", ".join([
+        f"'{key}': {value}"
+        for key, value in response.items()
+        if key != "dominant_emotion"
+    ])
+
+    return f"""For the given statement, the system response is {emotions}.
+        The dominant emotion is {response["dominant_emotion"]}
+    """
+
+@app.route("/")
+def render_index_page():
+    """
+    Render the main index page
+    """
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
