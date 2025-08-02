40747a75bf16883fd0a4581b893e86915cf56bc5
selenaschz <sele71098@gmail.com>
2025-08-02 14:06:35 +0200

    Implement an emotion detection - Task 2

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    """
+    Sends a text string to the Watson NLP EmotionPredict API for analysis.
+    Returns the text of analysis
+    """
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/"
+        "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = myobj, headers=headers, timeout=10)
+    return response.text 
